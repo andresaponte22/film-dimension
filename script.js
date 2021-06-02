2ea1c3495e38151672fda62772f7bbbc702132b5
Daryl Nauman <darylnauman@gmail.com>
2021-06-02 09:28:58 -0400

    Updated script.js file with detailed outline/structure for code

--- script.js
+++ script.js
@@ -1,3 +1,178 @@
+/** JAVASCRIPT CODE OUTLINE
+ * - variable declarations
+ * - functions
+ *       - initalise
+ *       - handle user submissions
+ *       - fetch API data
+ *       - localStorage
+ *       - display results to pages
+ * - event listeners
+ * - list functions to call upon page load
+ */
 
 
-// Insert Rotten Tomatoes API fetch here
+
+
+
+// ---- Variable declarations ---- 
+
+// Reference to important DOM elements
+// e.g. var [nameEl] = document.querySelector('#[id]');
+// TO DO: Add global variables that will reference DOM elements
+
+
+
+
+
+
+
+
+
+
+// Global Variables
+// TO DO: Define any variables with global scope excluding those referencing DOM elements already noted above
+
+
+
+
+
+
+
+
+
+// ---- Functions ---- 
+
+// Function - Initialise web app
+// TO DO: Create function that is run when page first loaded, e.g., get localStorage.
+
+
+
+
+
+
+
+
+
+// ---- Functions to handle user form submissions ---- 
+
+// Function - Handle form / search submit including calling search APIs
+// TO DO: Create function that handles user submission (e.g., get movie title OR genre, check not blank, call APIs)
+
+
+
+
+
+
+
+// ---- Functions to fetch API data  ---- 
+
+// Function - YouTube API fetch here - search the API
+// TO DO: Create YouTube API fetch
+
+
+
+
+
+
+
+
+
+
+// Function - Rotten Tomatoes API fetch here - search the API
+// TO DO: Create Rotten Tomates API fetch
+
+
+
+
+
+
+
+
+
+// ---- Function to update localStorage ---- 
+
+// Function - Update variables and localStorage related to saved/searched movie titles, then call function to display on page
+// TO DO: Create function that will update searched/saved movie list variables & localStorage then call funciton to update display on page
+
+
+
+
+
+
+
+
+
+// ---- Functions to update display on pages ---- 
+
+// Function - Display YouTube video to main page
+// TO DO: Create function that will add YouTube API result to main page
+
+
+
+
+
+
+
+
+
+
+// Function - Display Rotten Tomates movie critic reviews & scores to main page
+// TO DO: Create function that will add Rotten Tomates API reviews and scores to main page
+
+
+
+
+
+
+
+
+
+
+// Function - Display Rotten Tomates movie listing based on genre results on second page, clickable movie info
+// TO DO: Create function that will add Rotten Tomates API movie list based on genre, clickable to display on main page
+
+
+
+
+
+
+
+
+
+
+// Function - Display saved / searched (?) movies on main page 
+// TO DO: Create function that will display saved/searched movies to main page
+
+
+
+
+
+
+
+
+
+// ---- Event listeners ---- 
+
+// TO DO: Create event listeners 
+// e.g. languageButtonsEl.addEventListener('click', buttonClickHandler);
+
+
+
+
+
+
+
+
+
+// ---- Functions to call  ---- 
+
+// TO DO: List any functions that are to be called upon main page load
+
+
+
+
+
+
+
+
+
